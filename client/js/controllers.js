0eff3fcbf14b1b7a141c75a52a80edf7ca7a5284
treeplantedbystreams <davbros88@hotmail.com>
2016-03-22 15:55:29 -0500

    Changes made the following day

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,32 +1,34 @@
 //Logic for page actions here. 
 app.controller('MainController', ['$scope', function($scope){
- 
-
+    
     function shalom(){
     alert("Shalom!")
     
 };
-     
-     $scope.createAlert = function() {
+    $scope.createAlert = function() {
     var iseeit = $scope.write;
     alert(iseeit);
 };
     
+   $scope.addName= function(){
+        console.log("hello");
+        var david = document.createElement('david');
+        $scope.span.appendChild(david);
+      
+   };
     
-     function createAlert() {
-    var iseeit = $scope.write;
-    alert(iseeit);
-};
+   
      
-     
+    $scope.colorChange = function(){
+            var colors = ["Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood",                        "CadetBlue","Chartreuse","Chocolate","Coral"];               
+            $scope.theColor = colors[Math.floor(Math.random() * colors.length)];
+    };
 }]);
 
 
-  
-    function createAlert() {
-    var iseeit = $scope.write;
-    alert(iseeit);
+ function shalom(){
+    alert("Shalom!")
+    
 };
-     
-      
-     
+    
+  
